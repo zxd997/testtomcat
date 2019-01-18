afd5ce34cc759896b7fc8752950338aac8497ac2
zxd997 <1726211067@qq.com>
2019-01-18 14:37:28 +0800

    one

---
+++ src/main/java/com/example/testtomcat/TesttomcatApplication.java
@@ -0,0 +1,20 @@
+package com.example.testtomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class TesttomcatApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TesttomcatApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesttomcatApplication.class, args);
+    }
+
+}
+
